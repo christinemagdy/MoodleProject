7dfcd839419011549859a538d03a0df0e5915841
christinmagdy <christinemagdy07@gmail.com>
2018-04-17 13:39:58 +0200

    E-voting Plugin

---
+++ UserHelper.php
@@ -0,0 +1,42 @@
+<?php
+
+ require_once (dirname(__FILE__ ).'UserHelper.php');
+
+   echo "<h1> USER HELPER FOR E-VOTING PLUGIN </h1>";
+
+   echo "<h2> How to add E-Voting Activity </h2>";
+   echo "NOTE'YoU Must Login  As a teacher'";
+   echo "<p> 1) Click on login </p>";
+   echo "<p> 2) Enter your user name and your password and then Click 'login' </p>";
+   echo "<p> 3) Click on Site Home </p>";
+   echo "<p> 4) Click on Current Course </p>";
+   echo "<p> 5) Click on TC 'Test Christophe' </p>";
+   echo "<p> 6) Click on  turn editing on </p>";
+   echo "<p> 7) Choose the Annonces that you want and  
+                     Click 'Add an activity or resource '</p>";
+   echo "<p> 8) Select on E-Voting activity    </p>";
+   echo "<p> 9) Click on  'Add' </p>";
+
+   echo "</br>";
+
+  echo "<h3> How to create a new E-Voting poll </h3>";
+  echo "<p> 1) Click on Questions </p>";
+  echo "<p> 2) Write your Question  </p>";
+  echo "<p> 3) add choices  </p>";
+  echo "<p> 4) Click on  'Add Question'  </p>";
+  echo "</br>";
+
+  echo "<h3>As Student Enter To enter Voting On Button </h3>";
+  echo "<p> 1) click on QR Code </p>";
+  echo "<p> 2) enter Your URl as Student </p>";
+  echo "<p> 3)Enter Start button </p>";
+  echo "<p> 4)Select Time for voting</p>";
+  echo "<p> 5)Enter on any option You want it </p>";
+  echo "<p> 6)enter stop </p>";
+
+  echo "<h4> IF You want to reset Your option</h4>";
+  echo "<p> 1) click on button Reset</p>";
+  echo "<p> 2) Enter Option Again </p>";
+  echo "<p> 3) Enter Stop </p>";
+  echo "</br>";
+
